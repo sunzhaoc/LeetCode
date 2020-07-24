8af0046d38bdb3ee680d0eb00d3659b34a9ce653
sunzhaoc <sunzhaoc_2011@163.com>
2020-07-24 10:04:06 +0800

    master

--- 0001_03.py
+++ 0001_03.py
@@ -1,1 +1,18 @@
-dddddddwewe we e
+nums = [2, 7, 11, 15]
+target = 9
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        dict = {}
+        for i in range(len(nums)):
+            a = target - nums[i]
+            if a in dict:
+                return dict[a], i
+            else:
+                dict[nums[i]] = i
+
+
+K = Solution()
+RESULT = K.twoSum(nums, target)
+print(RESULT)
