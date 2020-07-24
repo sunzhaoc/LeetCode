de8ae5226d2350b456969aa42bb34bbfff1c789e
sunzhaoc <sunzhaoc_2011@163.com>
2020-07-24 20:08:00 +0800

    master

---
+++ 0007_01my.py
@@ -0,0 +1,8 @@
+'''
+@Author: your name
+@Date: 2020-07-24 13:07:22
+@LastEditTime: 2020-07-24 13:07:22
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \Leetcode\0007_01my.py
+'''
