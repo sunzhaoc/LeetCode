c3cdc620151ec13763cce6e229a443ea7c5399f5
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-09 19:50:08 +0800

    master

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+Description: 
+Version: 1.0
+Autuor: Vicro
+Date: 2020-10-07 21:27:26
+LastEditTime: 2020-10-08 21:53:05
+FilePath: \Leetcode\test.py
+'''
+# matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+
+for i in range(5, 10):
+    print(i)
