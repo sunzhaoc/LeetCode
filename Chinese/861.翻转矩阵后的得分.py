d7cf0a4a543f318424e14f1716c911743a3a8c6b
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-21 14:59:40 +0800

    master

--- Chinese/861.翻转矩阵后的得分.py
+++ Chinese/861.翻转矩阵后的得分.py
@@ -3,7 +3,7 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-19 15:11:19
-LastEditTime: 2020-11-19 15:21:56
+LastEditTime: 2020-11-19 16:02:41
 FilePath: \Leetcode\Chinese\861.翻转矩阵后的得分.py
 '''
 #
@@ -15,14 +15,31 @@ FilePath: \Leetcode\Chinese\861.翻转矩阵后的得分.py
 # @lc code=start
 class Solution:
     def matrixScore(self, A):
-        for row in A:
+        # Caculate Row
+        for row in range(len(A)):
             if A[row][0] == 0:
-            else:
-                continue
+                A[row] = [1 - tmp for tmp in A[row]]
+        
+        # Caculate Col
+        for col in range(1, len(A[0])): # you have already make the 1-st element of each row to 1, so you do not need to judge it
+            one_num = sum(A[row][col] for row in range(len(A)))
+            if one_num < len(A) - one_num: # Zero Num
+                for row in range(len(A)):
+                    A[row][col] = 1 - A[row][col]
 
+        ans = 0
+        # for row in range(len(A)):
+        #     for col in range(len(A[0])):
+        #         if A[row][col] == 1:
+        #             ans += 2**(len(A[0])-1-col)
+        for row in range(len(A)):
+            tempA = A[i][::-1]
+            item = for index, val in range(zip())
+        return ans
 
-sol = Solution([[0,0,1,1],[1,0,1,0],[1,1,0,0]])
-B = sol.matrixScore()
+                
+sol = Solution()
+B = sol.matrixScore([[0,1],[0,1],[0,1],[0,0]])
 print(B)
 # @lc code=end
 
--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-'''
-Description: 
-Version: 1.0
-Author: Vicro
-Date: 2020-10-07 21:27:26
-LastEditTime: 2020-11-19 14:28:32
-FilePath: \Leetcode\test.py
-'''
-A = [224, 234, 5]
-
-b = A.sort()
-
-print(b)
-print(A)
-
+A = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+B = zip(A)
+print(B)
