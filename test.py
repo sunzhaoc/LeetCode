d2fbed0bd783ee4d6a019c14911100138e04bd0d
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-24 09:53:24 +0800

    master

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-A = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
-B = zip(A)
-print(B)
+'''
+Description: 
+Version: 1.0
+Author: Vicro
+Date: 2020-10-07 21:27:26
+LastEditTime: 2020-11-23 15:26:13
+FilePath: \Leetcode\test.py
+'''
+import random
+
+def whichMountain():
+    mountain_list = ["东梁", "王喔咀", "冻山", "终南山", "跑马梁", "鳌山", "光头山+鹿角梁", "高山草甸"]
+    print(random.choice(mountain_list))
+
+whichMountain()
+
+
