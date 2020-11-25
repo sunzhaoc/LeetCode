d4939f1c41e08524c9716039d4429556a1cf9a82
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-25 22:30:42 +0800

    master

--- 621.任务调度器.py
+++ 621.任务调度器.py
@@ -3,7 +3,7 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-25 18:53:55
-LastEditTime: 2020-11-25 19:24:17
+LastEditTime: 2020-11-25 22:29:27
 FilePath: \Leetcode\621.任务调度器.py
 '''
 #
@@ -19,10 +19,17 @@ class Solution:
         num = []
         for i in temp:
             num.append(tasks.count(i))
-            
+        num.sort(key=lambda x: -x)
+
+        time = (num[0] - 1) * (n + 1) + 1
+
+        if num.count(num[0]) < n:
+            time += num.count(num[0])
+        
+
 
 sol = Solution()
-A = sol.leastInterval(["A","A","A","B","B","B"])
+A = sol.leastInterval(["A","A","A","B","B","B"], 2)
 print(A)
 # @lc code=end
 
