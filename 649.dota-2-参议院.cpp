bb92817ca76ea852944568bbc104bea661600a48
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-25 14:41:35 +0800

    master

---
+++ 649.dota-2-参议院.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-11-25 14:08:33
+ * @LastEditTime: 2020-11-25 14:09:36
+ * @FilePath: \Leetcode\649.dota-2-参议院.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=649 lang=cpp
+ *
+ * [649] Dota2 参议院
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        bool R = true, D = true
+    }
+};
+// @lc code=end
+
---
+++ test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-11-25 14:23:54
+ * @LastEditTime: 2020-11-25 14:39:48
+ * @FilePath: \Leetcode\test.cpp
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello" << endl;
+    system("pause");
+}
