c28070978cb1f5a23576b807e1a2efc214207ba7
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-29 21:55:14 +0800

    master

---
+++ 867.转置矩阵.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Description: 
+ * 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-11-29 21:53:23
+ * @LastEditTime: 2020-11-29 21:54:24
+ * @FilePath: \Leetcode\867.转置矩阵.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=867 lang=cpp
+ *
+ * [867] 转置矩阵
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int row = A.size();
+        int col = A[0].size();
+        
+    }
+};
+// @lc code=end
+
