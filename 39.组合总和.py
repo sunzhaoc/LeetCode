a8f31842abe412515d395dc7bdbc6d618fd91bca
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-30 19:45:48 +0800

    master

--- 39.组合总和.py
+++ 39.组合总和.py
@@ -3,7 +3,7 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-30 17:07:56
-LastEditTime: 2020-11-30 19:07:47
+LastEditTime: 2020-11-30 19:43:26
 FilePath: \Leetcode\39.组合总和.py
 '''
 #
@@ -17,21 +17,38 @@ class Solution:
     def combinationSum(self, candidates, target):
         res = []
         n = len(candidates)
-        candidates.sort()
 
-        def backTrack(level, list):
-            if sum(list) == target:
-                temp = sorted(list)
-                if temp not in res:
-                    res.append(temp)
-            
+        def backTrack(list, cha):
+            if cha - sum(list) == 0:
+                return res
+            if cha - sum(list) > 0:
+                res.append(list)
+
             for i in range(n):
-                if sum(list) < target:
-                    backTrack(i + 1, list + [candidates[i]])
+                backTrack(, cha - candidates[i])               
+
+        backTrack([], target)
+        return res
+
+
+        """Method 01"""
+        # res = []
+        # n = len(candidates)
+        # candidates.sort()
+
+        # def backTrack(level, list):
+        #     if sum(list) == target:
+        #         temp = sorted(list)
+        #         if temp not in res:
+        #             res.append(temp)
+            
+        #     for i in range(n):
+        #         if sum(list) < target:
+        #             backTrack(i + 1, list + [candidates[i]])
         
-        backTrack(0, [])
+        # backTrack(0, [])
         
-        return res    
+        # return res    
 
 
 sol = Solution()
