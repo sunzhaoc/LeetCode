a43b7e02c55c03687ff4362e08982c4236a7df21
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-30 21:34:44 +0800

    master

--- 39.组合总和.py
+++ 39.组合总和.py
@@ -3,7 +3,7 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-30 17:07:56
-LastEditTime: 2020-11-30 20:13:12
+LastEditTime: 2020-11-30 21:00:19
 FilePath: \Leetcode\39.组合总和.py
 '''
 #
@@ -15,22 +15,44 @@ FilePath: \Leetcode\39.组合总和.py
 # @lc code=start
 class Solution:
     def combinationSum(self, candidates, target):
-        """Method 03"""
+        """参考链接：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/ """
+
+        
+        """Method 03 剪枝"""
         res = []
         n = len(candidates)
-
-        def backTrack(cha, list):
+        candidates.sort()
+        def backTrack(cha, list, use_list):
             if cha == 0:
                 res.append(list)
-            for i in range(n):
-                if cha - candidates[i] >= 0:
-                    backTrack(cha - candidates[i], list + [candidates[i]])
-
-        backTrack(target, [])
+                return
+            for i in range(len(use_list)):
+                    if cha - use_list[i] < 0:
+                        break
+                    backTrack(cha - use_list[i], list + [use_list[i]], use_list[i:])
+        
+        backTrack(target, [], candidates)
         return res
         
         
-        """Method 02"""
+        """Method 03  排除一些多余的遍历步骤"""
+        # res = []
+        # n = len(candidates)
+
+        # def backTrack(cha, list, use_list):
+        #     if cha < 0:
+        #         return
+        #     if cha == 0:
+        #         res.append(list)
+        #         return
+        #     for i in range(len(use_list)):
+        #             backTrack(cha - use_list[i], list + [use_list[i]], use_list[i:])
+        
+        # backTrack(target, [], candidates)
+        # return res
+        
+        
+        """Method 02 用差值来做"""
         # res = []
         # n = len(candidates)
 
@@ -47,7 +69,7 @@ class Solution:
         # return res
 
 
-        """Method 01"""
+        """Method 01 正常的回溯法"""
         # res = []
         # n = len(candidates)
         # candidates.sort()
