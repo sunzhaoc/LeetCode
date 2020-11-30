e7f0f3fd09758a86482174318a0d11be6c70ecad
sunzhaoc <sunzhaoc_2011@163.com>
2020-11-30 15:58:29 +0800

    master

---
+++ 90.子集-ii.py
@@ -0,0 +1,25 @@
+'''
+Description: 
+Version: 1.0
+Author: Vicro
+Date: 2020-11-30 15:56:54
+LastEditTime: 2020-11-30 15:58:23
+FilePath: \Leetcode\90.子集-ii.py
+'''
+#
+# @lc app=leetcode.cn id=90 lang=python3
+#
+# [90] 子集 II
+#
+
+# @lc code=start
+class Solution:
+    def subsetsWithDup(self, nums):
+
+
+sol = Solution()
+A = sol.subsetsWithDup([1,2,2])
+print(A)
+
+# @lc code=end
+
