de5f5dd56c6b4ae7fbe5caf08197b57e1f408c51
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-01 16:15:51 +0800

    master

--- 0.test.py
+++ 0.test.py
@@ -3,7 +3,7 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-24 22:21:23
-LastEditTime: 2020-12-01 13:54:07
+LastEditTime: 2020-12-01 16:09:10
 FilePath: \Leetcode\0.test.py
 '''
 
@@ -16,3 +16,6 @@ print(A)
 
 print((time.time() - start)*1000, "ms")
 """
+
+A = [1, 2, 3]
+print(A[-2])
