bbadb9f0110dbd49f8f92e5f56b74c8596fc060e
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-01 14:54:38 +0800

    master

--- 0.test.py
+++ 0.test.py
@@ -3,11 +3,16 @@ Description:
 Version: 1.0
 Author: Vicro
 Date: 2020-11-24 22:21:23
-LastEditTime: 2020-12-01 12:50:13
+LastEditTime: 2020-12-01 13:54:07
 FilePath: \Leetcode\0.test.py
 '''
-A = [1, 2, 3]
 
-B = A
-A.pop()
-C = 1
+"""
+import time
+start = time.time()
+sol = Solution()
+A = sol.()
+print(A)
+
+print((time.time() - start)*1000, "ms")
+"""
---
+++ 941.Valid Mountain Array.cpp
@@ -0,0 +1,63 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-01 14:03:40
+ * @LastEditTime: 2020-12-01 14:53:08
+ * @FilePath: \Leetcode\941.Valid Mountain Array.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=941 lang=cpp
+ *
+ * [941] 有效的山脉数组
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if (arr.size() < 3) return false;
+        int i = 0;
+        int n = arr.size();
+        while (i + 1 < n - 1  && arr[i] < arr[i + 1])
+        {
+            i ++;
+        }
+        
+        if (i == 0){
+            return false;
+        }
+
+        while (i + 1 < n && arr[i] > arr[i + 1])
+        {
+            i ++;
+        }
+        
+        if (i == n - 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+    }
+};
+
+int main(){
+    Solution sol1;
+    vector<int> arr = {1, 2, 2, 1};
+    bool A = sol1.validMountainArray(arr);
+    cout << A << endl;
+    system("pause");
+    return 0;
+}
+// @lc code=end
+
