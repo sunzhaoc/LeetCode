130311751922a9357128c50a350c3609b2271e36
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-01 09:53:02 +0800

    master

---
+++ 34.Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-01 09:37:48
+ * @LastEditTime: 2020-12-01 09:52:21
+ * @FilePath: \Leetcode\34.Find First and Last Position of Element in Sorted Array.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+    }
+};
+// @lc code=end
+
