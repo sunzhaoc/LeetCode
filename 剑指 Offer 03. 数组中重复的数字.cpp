3ef5965d12b851ac64cb7f5e6ac847fdb2df2ea5
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-03 22:11:50 +0800

    master

---
+++ 剑指 Offer 03. 数组中重复的数字.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-03 21:04:26
+ * @LastEditTime: 2020-12-03 21:50:02
+ * @FilePath: \Leetcode\剑指 Offer 03. 数组中重复的数字.cpp
+ */
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME: 108ms BEAT 51.19%
+MEMORY: 28.3MB BEAT 5.01%
+Description: 
+*/
+// class Solution {
+// public:
+//     int findRepeatNumber(vector<int> nums) {
+//         unordered_map <int, int> list; 
+//         for (int i = 0; i < nums.size(); i++){
+//             if(++list[nums[i]] > 1) return nums[i];
+//         }
+//         return -1;
+//     }
+// };
+
+
+int main(){
+    Solution sol;
+    int A = sol.findRepeatNumber({2, 3, 1, 0, 2, 5, 3});
+    cout << A << endl;
+    system("pause");
+    return 0;
+}
---
+++ 剑指 Offer 05. 替换空格.cpp
@@ -0,0 +1,43 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-03 21:50:54
+ * @LastEditTime: 2020-12-03 22:11:32
+ * @FilePath: \Leetcode\剑指 Offer 05. 替换空格.cpp
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+
+/*
+RESULT: Accept
+TIME: 0ms BEAT 100.00%
+MEMORY: 6.5MB BEAT 14.32%
+Description: 从前往后遍历数组，开辟一个新的字符串，遇到空格就append %20，希望能在原来的字符串上进行修改。
+*/
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string res = "";
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == ' ') res.append("%20");
+            else res += s[i];
+        }
+        return res;
+    }
+};
+
+
+int main(){
+    Solution sol;
+    string A = sol.replaceSpace("We are happy.");
+    cout << A << endl;
+    system("pause");
+    return 0;
+}
