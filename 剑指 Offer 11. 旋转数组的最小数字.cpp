f50c4297bd142891dfa600f541482ca059213fd4
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-03 22:48:28 +0800

    master

---
+++ 剑指 Offer 11. 旋转数组的最小数字.cpp
@@ -0,0 +1,31 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-03 22:17:37
+ * @LastEditTime: 2020-12-03 22:27:46
+ * @FilePath: \Leetcode\剑指 Offer 11. 旋转数组的最小数字.cpp
+ */
+
+
+/*
+RESULT: Accept
+TIME: 4ms BEAT 99.63%
+MEMORY: 12.4MB BEAT 5.15%
+Description: 二分法
+*/
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int l = 0, r = numbers.size() - 1;
+
+        while (l < r)
+        {
+            int mid = (r - l) / 2 + l;
+            if (numbers[mid] < numbers[r])  r = mid;
+            else if (numbers[mid] > numbers[r]) l = mid + 1;
+            else r--;
+        }
+        return numbers[l];
+    }
+};
---
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字.cpp
@@ -0,0 +1,32 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-03 22:29:07
+ * @LastEditTime: 2020-12-03 22:43:42
+ * @FilePath: \Leetcode\剑指 Offer 53 - II. 0～n-1中缺失的数字.cpp
+ */
+
+/*
+RESULT: Accept
+TIME: 32ms BEAT 95.24%
+MEMORY: 17.2MB BEAT 16.24%
+Description: 二分法
+*/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        
+        while (l < r)
+        {
+            int mid = (r - l) / 2 + l;
+
+            if (nums[mid] == mid)   l = mid + 1;
+            else r = mid;
+        }
+
+        if (nums[l] == l) return ++l;
+        else return l;
+    }
+};
