b67d58894f9534be188f6a5520b77f8f844ed11e
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-04 20:53:27 +0800

    master

--- 剑指 Offer 64. 求1+2+…+n.cpp
+++ 剑指 Offer 64. 求1+2+…+n.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Author: Vicro
  * @Date: 2020-12-04 17:14:13
- * @LastEditTime: 2020-12-04 17:15:10
+ * @LastEditTime: 2020-12-04 20:49:30
  * @FilePath: \Leetcode\剑指 Offer 64. 求1+2+…+n.cpp
  */
 #include <iostream>
@@ -20,7 +20,7 @@ public:
 
 int main(){
     Solution sol;
-    int ans = sol.sumNums(2);
+    int ans = sol.sumNums(9);
     cout << ans << endl;
     system("pause");
     return 0;
