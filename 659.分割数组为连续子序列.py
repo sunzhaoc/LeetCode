a11945c46e5bfa9b10026ea0a5e7f9e635d7de00
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-04 14:09:12 +0800

    master

--- 659.分割数组为连续子序列.py
+++ 659.分割数组为连续子序列.py
@@ -1,3 +1,11 @@
+'''
+Description: 
+Version: 1.0
+Author: Vicro
+Date: 2020-12-04 13:18:25
+LastEditTime: 2020-12-04 14:08:20
+FilePath: \Leetcode\659.分割数组为连续子序列.py
+'''
 #
 # @lc app=leetcode.cn id=659 lang=python3
 #
@@ -5,7 +13,48 @@
 #
 
 # @lc code=start
+
+"""
+RESULT: Accept
+TIME: 40ms BEAT 88.02%
+MEMORY: 14.5MB BEAT 28.29%
+Description: 
+"""
 class Solution:
-    def isPossible(self, nums: List[int]) -> bool:
+    def isPossible(self, nums):
+        dict = {}
+        for i in nums:
+            if i not in dict.keys():
+                dict[i] = 1
+            else:
+                dict[i] += 1
+        tail = {}
+        
+        for i in nums:
+            if dict[i] == 0: continue
+            if (i - 1) in tail.keys() and tail[i - 1] > 0 and dict[i] > 0:
+                dict[i] -= 1
+                tail[i - 1] -= 1
+                if i in tail.keys():
+                    tail[i] += 1
+                else:
+                    tail[i] = 1
+            elif (i in dict.keys() and (i + 1) in dict.keys() and (i + 2) in dict.keys()) and dict[i] > 0 and dict[i + 1] > 0 and dict[i + 2] > 0:
+                dict[i] -= 1
+                dict[i + 1] -= 1
+                dict[i + 2] -= 1
+                if (i + 2) in tail.keys():
+                    tail[i + 2] += 1
+                else: 
+                    tail[i + 2] = 1
+            else: return False
+        return True
+                
+                
+
+sol = Solution()
+A = sol.isPossible([1,2,5,5,6,6,7,8,8,9])
+print(A)
+
 # @lc code=end
 
