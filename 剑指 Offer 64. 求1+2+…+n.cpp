74d80b199617032734730bff48e7788cd423755d
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-04 17:19:31 +0800

    master

---
+++ 剑指 Offer 64. 求1+2+…+n.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Vicro
+ * @Date: 2020-12-04 17:14:13
+ * @LastEditTime: 2020-12-04 17:15:10
+ * @FilePath: \Leetcode\剑指 Offer 64. 求1+2+…+n.cpp
+ */
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int sumNums(int n) {
+
+    }
+};
+
+
+int main(){
+    Solution sol;
+    int ans = sol.sumNums(2);
+    cout << ans << endl;
+    system("pause");
+    return 0;
+}
