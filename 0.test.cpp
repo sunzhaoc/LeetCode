7458a627d9d2dcc978c3ba454ac2a27ac021936c
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-09 15:38:44 +0800

    master

--- 0.test.cpp
+++ 0.test.cpp
@@ -1,47 +1,33 @@
 /*
- * @Description:  
+ * @Description: 
  * @Version: 1.0
  * @Author: Vicro
- * @Date: 2020-12-09 10:28:43
- * @LastEditTime: 2020-12-09 10:40:37
+ * @Date: 2020-11-25 15:06:00
+ * @LastEditTime: 2020-12-09 14:58:32
  * @FilePath: \Leetcode\0.test.cpp
  */
-/*
- * @lc app=leetcode.cn id=62 lang=cpp
- *
- * [62] 不同路径
- */
-
-// @lc code=start
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        long long sum_up = 1;
-        int sum_down = 1;
-
-        int max = (n >= m)? n : m;
-        int min = (n <= m)? n : m;
-        for (int i = max; i < min + max - 2 + 1; i ++) {
-            sum_up = i * sum_up;
-        }
-        for (int i = 1; i < min - 1 + 1; i ++) {
-            sum_down = i * sum_down;
-        }
-        return sum_up / sum_down;
-    }
-};
-
-
 int main() {
-    Solution sol;
-    int A = sol.uniquePaths(3, 2);
-    cout << A << endl;
+    vector<int> inorder = {1, 2, 3, 4, 5};
+    vector<int> preorder = {5, 4, 3, 2, 1};
+
+    // int mid;
+    // for (mid = 0; mid <= 4; mid ++) {
+    //     if (inorder[mid] == preorder[1]) break;
+    // }
+
+    int mid = 0;
+    while (mid <= 4) {
+        if (inorder[mid] == preorder[1])  break;
+        mid ++;
+    }
+
+    cout << mid << endl;
+
     system("pause");
     return 0;
-}
-// @lc code=end
-
+};
--- 105.从前序与中序遍历序列构造二叉树.cpp
+++ 105.从前序与中序遍历序列构造二叉树.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Author: Vicro
  * @Date: 2020-12-09 10:52:00
- * @LastEditTime: 2020-12-09 11:13:16
+ * @LastEditTime: 2020-12-09 15:08:28
  * @FilePath: \Leetcode\105.从前序与中序遍历序列构造二叉树.cpp
  */
 /*
@@ -27,36 +27,77 @@ struct TreeNode {
 
 /*
 RESULT: Accept
-TIME:     328ms    BEAT 5.16%    O(n) = 
-MEMORY: 149.9MB    BEAT 5.02%    O(n) =  
+TIME:     48ms    BEAT 34.73%    O(n) = n
+MEMORY: 17.3MB    BEAT 60.56%    O(n) = n
 Description: 
 */
 
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int m = preorder.size();
-        if (m == 0) return NULL;
+    unorder_map<int, int> idx_map;
+    int num = 0;
 
-        TreeNode* root = new TreeNode(preorder[0]);
-        vector<int> preorder_left, inorder_left, preorder_right, inorder_right;
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int left, int right) {
+        if (left > right)   return nullptr;
+        
 
-        int i;
-        for (i = 0; i < m; i ++) {
-            if (inorder[i] == preorder[0])  break;
-            inorder_left.push_back(inorder[i]);
-        }
-        for (i = i + 1; i < m; i ++) {
-            inorder_right.push_back(inorder[i]);
-        }
-        for (int j = 1; j < m; j ++) {
-            if (j <= inorder_left.size())    preorder_left.push_back(preorder[j]);
-            else preorder_right.push_back(preorder[j]);
-        }
-        root->left = buildTree(preorder_left, inorder_left);
-        root->right = buildTree(preorder_right, inorder_right);
+        root->left = helper(preorder, l1 + 1, l1 + mid, inorder, l2, l2 + mid - 1);
+        root->right = helper(preorder, l1 + mid + 1, r1, inorder, l2 + mid + 1, r2);
         return root;
     }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.empty())   return nullptr;
+
+        // 存储每个元素的位置
+        for (int i = 0; i < inorder.size(); i ++) {
+            idx_map[inorder[i]] = i;
+        }
+
+        return helper(preorder, inorder, 0, preorder.size() - 1);
+    }
 };
+
+
+/*
+RESULT: Accept
+TIME:     328ms    BEAT 5.16%    O(n) = 
+MEMORY: 149.9MB    BEAT 5.02%    O(n) =  
+Description: 
+*/
+
+// class Solution {
+// public:
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         int m = preorder.size();
+//         if (m == 0) return NULL;
+
+//         TreeNode* root = new TreeNode(preorder[0]);
+//         vector<int> preorder_left, inorder_left, preorder_right, inorder_right;
+
+//         int i;
+//         // 1、构建左子树的中序遍历
+//         for (i = 0; i < m; i ++) {
+//             if (inorder[i] == preorder[0])  break;
+//             inorder_left.push_back(inorder[i]);
+//         }
+
+//         // 2、构建右子树的中序遍历
+//         for (i = i + 1; i < m; i ++) {
+//             inorder_right.push_back(inorder[i]);
+//         }
+
+//         // 3、构建左、右子树的前序遍历
+//         for (int j = 1; j < m; j ++) {
+//             if (j <= inorder_left.size())    preorder_left.push_back(preorder[j]);
+//             else preorder_right.push_back(preorder[j]);
+//         }
+
+//         root->left = buildTree(preorder_left, inorder_left);
+//         root->right = buildTree(preorder_right, inorder_right);
+//         return root;
+//     }
+// };
+
 // @lc code=end
 
