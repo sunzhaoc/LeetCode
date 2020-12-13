2a3a9ef8587226e2047324fd4b2ccd17d7388db0
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-13 19:28:40 +0800

    colltecion

---
+++ 217.存在重复元素.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=217 lang=python3
+#
+# [217] 存在重复元素
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums):
+        
+
+sol = Solution()
+A = sol.containsDuplicate([1, 2, 3, 4])
+# @lc code=end
+
