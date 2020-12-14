7bf47947ec769061cf89916bdaed4d00bab80d2b
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-14 13:36:57 +0800

    master

---
+++ 49.字母异位词分组.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=49 lang=python3
+#
+# [49] 字母异位词分组
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+sol = Solution()
+A = sol.groupAnagrams
+# @lc code=end
+
