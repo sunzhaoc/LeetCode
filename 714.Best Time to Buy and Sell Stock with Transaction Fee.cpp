44e14bbf37518f44ba1d5f6ff36d9a292b3c1a5c
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-17 15:24:45 +0800

    master

---
+++ 714.Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=714 lang=cpp
+ *
+ * [714] 买卖股票的最佳时机含手续费
+ */
+
+// @lc code=start
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+
+    }
+};
+
+
+int main () {
+    Solution sol;
+    vector<int> prices = {1, 3, 2, 8, 4, 9};
+    int ans = sol.maxProfit(prices, 2);
+    cout << ans << endl;
+    system("pause");
+    return 0;
+}
+// @lc code=end
+
