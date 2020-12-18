285e2a2beee64139097e5e825131ce5174ba81ba
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-18 14:34:22 +0800

    master

---
+++ 389.Find the Difference.cpp
@@ -0,0 +1,65 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2020-12-18 12:43:34
+ * @LastEditTime: 2020-12-18 13:10:01
+ * @FilePath: \Leetcode\389.Find the Difference.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=389 lang=cpp
+ *
+ * [389] 找不同
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:     4ms    BEAT 73.81%    O(n) = n
+MEMORY: 6.9MB    BEAT 21.88%    O(n) = 1
+Description: 位运算。
+*/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int res = 0;
+        for (char ch: s)    res ^= ch;
+        for (char ch: t)    res ^= ch;
+        return res;
+        }
+};
+
+
+/*
+RESULT: Accept
+TIME:     4ms    BEAT 73.81%    O(n) = n
+MEMORY: 6.8MB    BEAT 45.80%    O(n) = 1
+Description: 求和。
+*/
+
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         int sum_s = 0, sum_t = 0;
+//         for (char temp: s) sum_s += temp;
+//         for (char temp: t) sum_t += temp;
+//         return sum_t - sum_s;
+//     }
+// };
+
+
+int main() {
+    Solution sol;
+    char ans = sol.findTheDifference("abcd", "abcde");
+    cout << ans << endl;
+    system("pause");
+    return 0;
+}
+
+// @lc code=end
+
--- 714.Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 714.Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,4 +1,12 @@
 /*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2020-12-17 15:15:15
+ * @LastEditTime: 2020-12-17 20:19:27
+ * @FilePath: \Leetcode\714.Best Time to Buy and Sell Stock with Transaction Fee.cpp
+ */
+/*
  * @lc app=leetcode.cn id=714 lang=cpp
  *
  * [714] 买卖股票的最佳时机含手续费
@@ -11,10 +19,30 @@
 using namespace std;
 
 
+/*
+RESULT: Accept
+TIME:    196ms    BEAT 91.69%    O(n) = n
+MEMORY: 50.9MB    BEAT 65.52%    O(n) = 1
+Description: 贪心。
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
+        int buy = prices[0] + fee;
+        int profit = 0;
 
+        for(int i = 1; i < prices.size(); i ++) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            }
+            else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        
+        return profit;
     }
 };
 
