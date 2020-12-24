968d39316ddc80813a88735c97d58685d9bbf5b9
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-24 09:24:00 +0800

    master

---
+++ 135.分发糖果.py
@@ -0,0 +1,20 @@
+'''
+Description: 
+Version: 1.0
+Autor: Vicro
+Date: 2020-12-24 09:16:30
+LastEditTime: 2020-12-24 09:16:39
+FilePath: \Leetcode\135.分发糖果.py
+'''
+#
+# @lc app=leetcode.cn id=135 lang=python3
+#
+# [135] 分发糖果
+#
+
+# @lc code=start
+class Solution:
+    def candy(self, ratings):
+        
+# @lc code=end
+
