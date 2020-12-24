e7794a7973a38f442acbf1f26cfd824217eda0cf
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-24 17:10:11 +0800

    master

--- 229.Majority Element II.cpp
+++ 229.Majority Element II.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2020-12-24 15:39:47
- * @LastEditTime: 2020-12-24 15:45:56
+ * @LastEditTime: 2020-12-24 17:09:50
  * @FilePath: \Leetcode\229.Majority Element II.cpp
  */
 /*
@@ -20,15 +20,55 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT %    O(n) = 
-MEMORY: MB    BEAT %    O(n) = 
-Description: 
+TIME:     32ms    BEAT 69.75%    O(n) = 2n
+MEMORY: 15.7MB    BEAT 48.92%    O(n) = 1
+Description: 摩尔投票法。So Hard！本题是求个数大于长度1/3的所有数。假设数组长度为n,那么我们稍微思考可以得出结论个数超过3/n的数不会超过2个。
+https://leetcode-cn.com/problems/majority-element-ii/solution/liang-fu-dong-hua-yan-shi-mo-er-tou-piao-fa-zui-zh/
 */
 
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int ticket01 = 0, ticket02 = 0;
+        int person01 = nums[0], person02 = nums[0];
 
+        for (int mem: nums) {
+            if (person01 == mem) {
+                ticket01 ++;
+                continue;
+            }
+            
+            if (person02 == mem) {
+                ticket02 ++;
+                continue;
+            }
+            
+            if (ticket01 == 0) {
+                person01 = mem;
+                ticket01 ++;
+                continue;
+            }
+
+            if (ticket02 == 0) {
+                person02 = mem;
+                ticket02 ++;
+                continue;
+            }
+
+            ticket01 --;
+            ticket02 --;
+        }
+
+        int count1 = 0, count2 = 0;
+        for (int mem: nums) {
+            if (mem == person01) count1 ++;
+            else if (mem == person02) count2 ++;
+        }
+        
+        vector<int> res;
+        if (count1 > nums.size() / 3) res.push_back(person01);
+        if (count2 > nums.size() / 3) res.push_back(person02);
+        return res;
     }
 };
 
