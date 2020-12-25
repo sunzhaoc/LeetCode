62d44349b4ae3585619255f6c11286c0d7499ce0
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-25 11:22:58 +0800

    master

---
+++ 剑指 Offer 09. 用两个栈实现队列.cpp
@@ -0,0 +1,38 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2020-12-25 11:16:01
+ * @LastEditTime: 2020-12-25 11:16:10
+ * @FilePath: \Leetcode\剑指 Offer 09. 用两个栈实现队列.cpp
+ */
+
+
+/*
+RESULT: Accept
+TIME:   ms    BEAT %    O(n) = 
+MEMORY: MB    BEAT %    O(n) = 
+Description: 
+*/
+
+class CQueue {
+public:
+    CQueue() {
+
+    }
+    
+    void appendTail(int value) {
+
+    }
+    
+    int deleteHead() {
+
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
--- 剑指 Offer 09. 用两个栈实现队列.py
+++ 剑指 Offer 09. 用两个栈实现队列.py
@@ -3,9 +3,17 @@ Description:
 Version: 1.0
 Autor: Vicro
 Date: 2020-12-25 11:00:36
-LastEditTime: 2020-12-25 11:03:31
+LastEditTime: 2020-12-25 11:15:36
 FilePath: \Leetcode\剑指 Offer 09. 用两个栈实现队列.py
 '''
+
+"""
+RESULT: Accept
+TIME:    400ms    BEAT 96.04%    O(n) = 
+MEMORY: 18.3MB    BEAT 10.15%    O(n) = 
+Description: 
+"""
+
 class CQueue:
 
     def __init__(self):
@@ -16,7 +24,12 @@ class CQueue:
         self.A.append(value)
 
     def deleteHead(self) -> int:
-        
+        if self.B:  return self.B.pop()
+        if not self.A:  return -1
+        while self.A:
+            self.B.append(self.A.pop())
+        return self.B.pop()
+
 
 
 
