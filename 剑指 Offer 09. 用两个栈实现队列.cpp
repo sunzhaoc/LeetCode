be2c40c021a4aa382826e812c39124a235126499
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-30 13:25:30 +0800

    master

---
+++ 剑指 Offer 09. 用两个栈实现队列.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2020-12-30 13:24:55
+ * @LastEditTime: 2020-12-30 13:25:23
+ * @FilePath: \Leetcode\剑指 Offer 09. 用两个栈实现队列.cpp
+ */
+
+/*
+RESULT: Accept
+TIME:   ms    BEAT %    O(n) = 
+MEMORY: MB    BEAT %    O(n) = 
+Description: 
+*/
+
+class CQueue {
+public:
+    CQueue() {
+
+    }
+    
+    void appendTail(int value) {
+
+    }
+    
+    int deleteHead() {
+
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
