977796b89a9779da8fbe2810cd566d4f0760c03c
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-30 13:57:51 +0800

    master

--- 剑指 Offer 09. 用两个栈实现队列.cpp
+++ 剑指 Offer 09. 用两个栈实现队列.cpp
@@ -3,9 +3,13 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2020-12-30 13:24:55
- * @LastEditTime: 2020-12-30 13:25:23
+ * @LastEditTime: 2020-12-30 13:41:58
  * @FilePath: \Leetcode\剑指 Offer 09. 用两个栈实现队列.cpp
  */
+
+#include <stack>
+using namespace std;
+
 
 /*
 RESULT: Accept
@@ -16,16 +20,28 @@ Description:
 
 class CQueue {
 public:
+    stack<int> stack1, stack2;
+
     CQueue() {
 
     }
     
     void appendTail(int value) {
-
+        stack1.push(value);
     }
-    
-    int deleteHead() {
 
+    int deleteHead() {
+        if (!stack2.empty()) {
+            int ans = stack2.top();
+            stack2.pop();
+            return ans;
+        }
+        if (stack1.empty()) return -1;
+        while (!stack1.empty()) {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+        return stack2.top();
     }
 };
 
