f53236e6914caf29b7e7338aecc9b4138e0420ee
sunzhaoc <sunzhaoc_2011@163.com>
2020-12-30 16:36:51 +0800

    master

--- 剑指 Offer 22. 链表中倒数第k个节点.cpp
+++ 剑指 Offer 22. 链表中倒数第k个节点.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2020-12-30 16:11:53
- * @LastEditTime: 2020-12-30 16:34:20
+ * @LastEditTime: 2020-12-30 16:34:56
  * @FilePath: \Leetcode\剑指 Offer 22. 链表中倒数第k个节点.cpp
  */
 
@@ -19,7 +19,7 @@ struct ListNode {
 RESULT: Accept
 TIME:      0ms    BEAT 100.00%    O(n) = n
 MEMORY: 10.8MB    BEAT  26.93%    O(n) = 1
-Description: 
+Description: 双指针。
 */
 
 class Solution {
@@ -27,9 +27,11 @@ public:
     ListNode* getKthFromEnd(ListNode* head, int k) {
         ListNode* point_left = head;
         ListNode* point_right = head;
+
         for (int i = 0; i < k; i ++) {
             point_right = point_right->next;
         }
+        
         while (point_right != nullptr) {
             point_left = point_left->next;
             point_right = point_right->next;
---
+++ 剑指 Offer 24. 反转链表.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2020-12-30 16:36:08
+ * @LastEditTime: 2020-12-30 16:36:09
+ * @FilePath: \Leetcode\剑指 Offer 24. 反转链表.cpp
+ */
