f1e39cf2812ff1b79b8a1ca21614a23b0f1710ee
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-04 14:35:44 +0800

    master

---
+++ 78.Subsets.cpp
@@ -0,0 +1,47 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-04 14:31:27
+ * @LastEditTime: 2021-01-04 14:33:54
+ * @FilePath: \Leetcode\78.Subsets.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:   ms    BEAT %    O(n) = 
+MEMORY: MB    BEAT %    O(n) = 
+USED TIME: 
+Description: 
+*/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+    }
+};
+
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> ans = sol.subsets(nums);
+    system("pause");
+    return 0;
+}
+
+// @lc code=end
+
