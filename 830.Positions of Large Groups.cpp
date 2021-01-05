66f33cb599f46a0fa864ecfdbd4dfa7cb58695d5
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-05 09:33:44 +0800

    master

---
+++ 830.Positions of Large Groups.cpp
@@ -0,0 +1,67 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-05 09:10:56
+ * @LastEditTime: 2021-01-05 09:24:30
+ * @FilePath: \Leetcode\830.Positions of Large Groups.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=830 lang=cpp
+ *
+ * [830] 较大分组的位置
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:     0ms    BEAT 100.00%    O(n) = n
+MEMORY: 7.7MB    BEAT  33.85%    O(n) = 1
+USED TIME: 13:06
+Description: 
+*/
+
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        int start = 0, end = 0;
+        vector<vector<int>> ans;
+        for (int i  = 1; i < s.length(); i ++) {
+            if (s[i] == s[i - 1]) {
+                end = i;
+                if (i == s.length() - 1 && end - start >= 2) ans.push_back({start, end});
+            }
+            else if (end - start >= 2) {
+                ans.push_back({start, end});
+                start = i;
+                end = i;
+            }
+            else {
+                start = i;
+                end = i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    // vector<vector<int>> ans = sol.largeGroupPositions("abbxxxxzzy");
+    // vector<vector<int>> ans = sol.largeGroupPositions("abcdddeeeeaabbbcd");
+    // vector<vector<int>> ans = sol.largeGroupPositions("aaa");
+    vector<vector<int>> ans = sol.largeGroupPositions("aa");
+    // cout << ans << endl;
+    system("pause");
+    return 0;
+}
+
+// @lc code=end
+
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2020-11-30 21:56:56
- * @LastEditTime: 2021-01-04 15:23:54
+ * @LastEditTime: 2021-01-05 09:18:36
  * @FilePath: \Leetcode\readme.md
 -->
 
@@ -46,5 +46,6 @@ using namespace std;
 int main() {
     Solution sol;
     cout << << endl;
+    system("pause");
     return 0;
 }
