d3b95a6f6ccb20f0b3be6436e2d5dd8be371eb94
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-07 09:46:12 +0800

    master

---
+++ 547.Number of Provinces.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-07 09:23:08
+ * @LastEditTime: 2021-01-07 09:23:08
+ * @FilePath: \Leetcode\547.Number of Provinces.cpp
+ */
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+    }
+};
