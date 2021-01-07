bc2a4bff87c9a084d7e715e83d6591bd70e9404c
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-07 17:42:32 +0800

    master

--- 449.Serialize and Deserialize BST.cpp
+++ 449.Serialize and Deserialize BST.cpp
@@ -1,4 +1,12 @@
 /*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-07 17:34:18
+ * @LastEditTime: 2021-01-07 17:42:05
+ * @FilePath: \Leetcode\449.Serialize and Deserialize BST.cpp
+ */
+/*
  * @lc app=leetcode.cn id=449 lang=cpp
  *
  * [449] 序列化和反序列化二叉搜索树
@@ -14,12 +22,15 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+
 class Codec {
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        
+        if (root == nullptr) return;
+        return to_string(root->val) + ' ' + serialize(root->left) + ' ' + serialize(root->right);
     }
 
     // Decodes your encoded data to tree.
