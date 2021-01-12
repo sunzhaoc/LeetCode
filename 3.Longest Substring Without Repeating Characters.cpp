87c3e33db79abea004d00b7fb556dd2cdee435a4
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-12 15:29:38 +0800

    master

--- 3.Longest Substring Without Repeating Characters.cpp
+++ 3.Longest Substring Without Repeating Characters.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-01-12 14:23:16
- * @LastEditTime: 2021-01-12 15:05:34
+ * @LastEditTime: 2021-01-12 15:11:58
  * @FilePath: \Leetcode\3.Longest Substring Without Repeating Characters.cpp
  */
 /*
@@ -20,14 +20,36 @@
 #include <algorithm>
 #include <math.h>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 
+/*
+RESULT: Accept
+TIME:   ms    BEAT: %    O(n) = 
+MEMORY: MB    BEAT: %    O(n) = 
+Description: 官解。
+*/
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        unordered_set<char> occ;
+        int n = s.length();
         
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; i ++) {
+            if (i != 0) occ.erase(s[i - 1]);
+            
+            while (rk + 1 < n && !occ.count(s[rk + 1])) {
+                occ.insert(s[rk + 1]);
+                ++ rk;
+            }
+
+            ans = max(ans, rk - i + 1);
+        }
+        
+        return ans;        
     }
 };
 
@@ -40,41 +62,41 @@ USED TIME: 30:22
 Description: 
 */
 
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        if (s.length() == 0) return 0;
-        if (s.length() == 1) return 1;
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         if (s.length() == 0) return 0;
+//         if (s.length() == 1) return 1;
         
-        unordered_map<int, int> map;
-        int left = 0, right = 0;
-        int max_length = 0;
+//         unordered_map<int, int> map;
+//         int left = 0, right = 0;
+//         int max_length = 0;
 
-        for (int i = 0; i < s.length(); i ++) {
-            if (map.find(s[i]) == map.end()) {
-                map[s[i]] = i;
-                max_length = right - left + 1 > max_length ? right - left + 1 : max_length;
-                right ++;
-            }
-            else {
-                auto it = map.find(s[i]);
-                if (it->second < left) {
-                    map[s[i]] = i;
-                    if (i == s.length() - 1) max_length = right - left + 1 > max_length ? right - left + 1 : max_length;
-                    right ++;
+//         for (int i = 0; i < s.length(); i ++) {
+//             if (map.find(s[i]) == map.end()) {
+//                 map[s[i]] = i;
+//                 max_length = right - left + 1 > max_length ? right - left + 1 : max_length;
+//                 right ++;
+//             }
+//             else {
+//                 auto it = map.find(s[i]);
+//                 if (it->second < left) {
+//                     map[s[i]] = i;
+//                     if (i == s.length() - 1) max_length = right - left + 1 > max_length ? right - left + 1 : max_length;
+//                     right ++;
 
-                }
-                else { // it->second >= left;
-                    max_length = (right - 1 - left + 1) > max_length ? (right - 1 - left + 1) : max_length;
-                    left = it->second + 1;
-                    right = i + 1;
-                    map[s[i]] = i;
-                }
-            }
-        }
-        return max_length;
-    }
-};
+//                 }
+//                 else { // it->second >= left;
+//                     max_length = (right - 1 - left + 1) > max_length ? (right - 1 - left + 1) : max_length;
+//                     left = it->second + 1;
+//                     right = i + 1;
+//                     map[s[i]] = i;
+//                 }
+//             }
+//         }
+//         return max_length;
+//     }
+// };
 
 
 int main() {
