34bb6815d18f9feecfb212a87c2a143c367230ee
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-13 16:35:28 +0800

    master

--- 15.3Sum.cpp
+++ 15.3Sum.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-01-13 14:23:17
- * @LastEditTime: 2021-01-13 14:30:10
+ * @LastEditTime: 2021-01-13 16:33:44
  * @FilePath: \Leetcode\15.3Sum.cpp
  */
 /*
@@ -13,6 +13,11 @@
  */
 
 // @lc code=start
+
+/*
+本来是参考01题两数之和，打算用哈希，但是去重相当麻烦。可参考下面的。
+https://leetcode-cn.com/problems/3sum/solution/15-san-shu-zhi-he-ha-xi-fa-shuang-zhi-zhen-fa-xi-2/
+*/
 
 #include <iostream>
 #include <string>
@@ -25,26 +30,90 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
-USED TIME: 
-Description: 
+TIME:   132ms    BEAT: 46.20%    O(n) = n^2
+MEMORY:  20MB    BEAT: 49.29%    O(n) = logn
+Description: 双指针。
 */
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
 
+            int k = nums.size() - 1;
+            int j = i + 1;
+
+            while (j < k) {
+                if (nums[j] + nums[k] > - nums[i]) k --;
+                else if (nums[j] + nums[k] < - nums[i]) j ++;
+                else {
+                    res.push_back({nums[i], nums[j], nums[k]});
+
+                    while (k > j && nums[k] == nums[k - 1]) k --;
+                    while (k > j && nums[j] == nums[j + 1]) j ++;
+
+                    j ++;
+                    k --;
+                }
+            }
+        }
+        return res;
     }
 };
 
 
+/*
+RESULT: Accept
+TIME:    112ms    BEAT: 72.11%    O(n) = n^2 这个的时间复杂度可以看看。
+MEMORY: 20.1MB    BEAT: 35.14%    O(n) = logn
+USED TIME: 1:16:37
+Description: 双指针。
+*/
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> res;
+//         int target = 0;
+
+//         for (int i = 0; i < nums.size(); i ++) {
+//             if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+//             target = 0 - nums[i];
+//             int k = nums.size() - 1;
+
+//             for (int j = i + 1; j < nums.size(); j ++) {
+//                 if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+
+//                 while (j < k && nums[j] + nums[k] > target) k --;
+
+//                 if (j == k) {
+//                     break;
+//                 }
+                
+//                 if (nums[j] + nums[k] == target) {
+//                     res.push_back({nums[i], nums[j], nums[k]});
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
 int main() {
     Solution sol;
-    vector<int> nums = {-1,0,1,2,-1,-4};
+    // vector<int> nums = {-1,0,1,2,-1,-4};
+    // vector<int> nums = {0,0,0};
+    vector<int> nums = {0,2,2,3,0,1,2,3,-1,-4,2};
+    // vector<int> nums = {-5,1,-3,-1,-4,-2,4,-1,-1};
     vector<vector<int>> ans = sol.threeSum(nums);
     for (int i = 0; i < ans.size(); i ++) {
-        cout << ans[i][0] << ans[i][1] << ans[i][2] << endl;
+        cout << ans[i][0] << " "<< ans[i][1] << " " << ans[i][2] << endl;
     }
     system("pause");
     return 0;
---
+++ 18.4Sum.cpp
@@ -0,0 +1,41 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-13 16:33:09
+ * @LastEditTime: 2021-01-13 16:33:20
+ * @FilePath: \Leetcode\18.四数之和.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=18 lang=cpp
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:   ms    BEAT: %    O(n) = 
+MEMORY: MB    BEAT: %    O(n) = 
+USED TIME: 
+Description: 
+*/
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+    }
+};
+// @lc code=end
+
