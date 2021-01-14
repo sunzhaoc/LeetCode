d8ccdc12d6978f8a2b57f40a2530efdc32545c9f
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-14 15:25:05 +0800

    master

---
+++ 1018.Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,67 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-01-14 14:27:39
+ * @LastEditTime: 2021-01-14 14:39:39
+ * @FilePath: \Leetcode\1018.Binary Prefix Divisible By 5.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=1018 lang=cpp
+ *
+ * [1018] 可被 5 整除的二进制前缀
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:      8ms    BEAT: 100.00%    O(n) = n
+MEMORY: 13.1MB    BEAT:  99.77%    O(n) = n
+USED TIME: 06:51
+LAST EDIT TIME: 2021年1月14日14:38:14
+Description: 同余定理。
+*/
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+        vector<bool> res;
+
+        int tmp = 0;
+        for (int i = 0; i < A.size(); i ++) {
+            tmp = (tmp * 2 + A[i]) % 5;
+            if (tmp == 0) res.push_back(true);
+            else res.push_back(false);
+        }
+
+        return res;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    // vector<int> A = {0,1,1};
+    // vector<int> A = {1,1,1};
+    vector<int> A = {0,1,1,1,1,1};
+    // vector<int> A = {1,1,1,0,1};
+
+    vector<bool> ans = sol.prefixesDivBy5(A);
+    for (int i = 0; i < ans.size(); i ++) {
+        cout << ans[i] << endl;
+    }
+    system("pause");
+    return 0;
+}
+// @lc code=end
+
--- 167.Two Sum II - Input array is sorted.cpp
+++ 167.Two Sum II - Input array is sorted.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-01-13 17:08:29
- * @LastEditTime: 2021-01-13 17:19:52
+ * @LastEditTime: 2021-01-13 17:32:45
  * @FilePath: \Leetcode\167.Two Sum II - Input array is sorted.cpp
  */
 /*
@@ -24,6 +24,34 @@ using namespace std;
 
 /*
 RESULT: Accept
+TIME:    12ms    BEAT: 77.21%    O(n) = nlogn
+MEMORY: 9.7MB    BEAT: 50.60%    O(n) = 1
+USED TIME: 04:42
+LAST EDIT TIME: 2021年1月13日17:32:1
+Description: 二分查找。
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l, r, mid;
+        for (int i = 0; i < numbers.size(); i ++) {
+            l = i + 1;
+            r = numbers.size() - 1;
+            while (l <= r) {
+                mid = (r - l) / 2 + l;
+                if (numbers[mid] + numbers[i] == target) return {i + 1, mid + 1};
+                else if (numbers[mid] + numbers[i] < target) l = mid + 1;
+                else r = mid - 1;
+            }
+        }
+        return {};
+    }
+};
+
+
+/*
+RESULT: Accept
 TIME:    12ms    BEAT: 77.21%    O(n) = n
 MEMORY: 9.7MB    BEAT: 37.39%    O(n) = 1 因为给定的数组是排序好的，所以不需要额外的空间去排序。
 USED TIME: 05:23
@@ -31,29 +59,29 @@ LAST EDIT TIME: 2021年1月13日17:18:5
 Description: 双指针。
 */
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int index1 = 0;
-        int index2 = numbers.size() - 1;
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int index1 = 0;
+//         int index2 = numbers.size() - 1;
 
-        while (index1 < index2) {
-            if (numbers[index1] + numbers[index2] < target) index1 ++;
-            else if (numbers[index1] + numbers[index2] > target) index2 --;
-            else {
-                if (numbers[index1] + numbers[index2] == target) {
-                    return {index1 + 1, index2 + 1};
-                }
-                while (index1 < index2 && numbers[index1 + 1] == numbers[index1]) index1 ++;
-                while (index1 < index2 && numbers[index2 - 1] == numbers[index2]) index2 --;
+//         while (index1 < index2) {
+//             if (numbers[index1] + numbers[index2] < target) index1 ++;
+//             else if (numbers[index1] + numbers[index2] > target) index2 --;
+//             else {
+//                 if (numbers[index1] + numbers[index2] == target) {
+//                     return {index1 + 1, index2 + 1};
+//                 }
+//                 while (index1 < index2 && numbers[index1 + 1] == numbers[index1]) index1 ++;
+//                 while (index1 < index2 && numbers[index2 - 1] == numbers[index2]) index2 --;
 
-                index1 ++;
-                index2 --;
-            }
-        }
-        return {};
-    }
-};
+//                 index1 ++;
+//                 index2 --;
+//             }
+//         }
+//         return {};
+//     }
+// };
 
 
 int main() {
