a8689b84486f42d9652609245ada1802e8a4ca7c
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-22 16:22:48 +0800

    master

--- 695.Max Area of Island.cpp
+++ 695.Max Area of Island.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-01-22 16:07:09
- * @LastEditTime: 2021-01-22 16:07:34
+ * @LastEditTime: 2021-01-22 16:22:21
  * @FilePath: \Leetcode\695.Max Area of Island.cpp
  */
 /*
@@ -25,17 +25,37 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
-USED TIME: 
-LAST EDIT TIME: 
-Description: 
+TIME:     20ms    BEAT: 96.10%    O(n) = 
+MEMORY: 21.7MB    BEAT: 98.90%    O(n) = 
+USED TIME: 10:07
+LAST EDIT TIME: 2021年1月22日16:22:4
+Description: DFS。
 */
 
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int maxIsland = 0;          
+    int tmp;
+    void dfs(vector<vector<int>>& grid, int x, int y) {
+        grid[x][y] = 0;
+        tmp ++;
 
+        if (x - 1 >= 0 && grid[x - 1][y] == 1) dfs(grid, x - 1, y);
+        if (x + 1 < grid.size() && grid[x + 1][y] == 1) dfs(grid, x + 1, y);
+        if (y - 1 >= 0 && grid[x][y - 1] == 1) dfs(grid, x, y - 1);
+        if (y + 1 < grid[0].size() && grid[x][y + 1] == 1) dfs(grid, x, y + 1); 
+        maxIsland = max(tmp, maxIsland);
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        for (int i = 0; i < grid.size(); i ++) {
+            for (int j = 0; j < grid[0].size(); j ++) {
+                if (grid[i][j] == 0) continue;
+                tmp = 0;
+                dfs(grid, i, j);
+            }
+        }
+        return maxIsland;
     }
 };
 
