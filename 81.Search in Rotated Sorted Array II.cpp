829849bcefde85efe56dfa3718117ac088417450
sunzhaoc <sunzhaoc_2011@163.com>
2021-01-30 21:41:16 +0800

    master

--- 81.Search in Rotated Sorted Array II.cpp
+++ 81.Search in Rotated Sorted Array II.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-01-30 18:24:57
- * @LastEditTime: 2021-01-30 20:17:33
+ * @LastEditTime: 2021-01-30 21:40:23
  * @FilePath: \Leetcode\81.Search in Rotated Sorted Array II.cpp
  */
 /*
@@ -25,10 +25,10 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
-LAST EDIT TIME: 
-Description: 
+TIME:     12ms    BEAT: 15.17%    O(n) = 
+MEMORY: 13.6MB    BEAT: 21.63%    O(n) = 
+LAST EDIT TIME: 2021年1月30日21:9:43
+Description: 二分查找。边界条件有点难找。
 */
 
 class Solution {
@@ -42,14 +42,31 @@ public:
             mid = (right - left) / 2 + left;
 
             if (nums[mid] == target) return true;
-            if (nums[left] == nums[right]) {
+            
+            if (nums[left] == nums[mid]) {
                 left ++;
                 continue;
             }
 
             // Front
-            if (nums[left] <= nums[mid])
+            if (nums[left] < nums[mid]) {
+                if (nums[left] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
         }
+        return false;
     }
 };
 // @lc code=end
