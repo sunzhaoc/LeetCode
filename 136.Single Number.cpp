38652beb80ef8ce1019c3dab3101a88e509cc95c
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-05 08:37:56 +0800

    master

--- 136.Single Number.cpp
+++ 136.Single Number.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Author: Vicro
  * @Date: 2020-12-02 21:14:42
- * @LastEditTime: 2021-01-06 16:49:51
+ * @LastEditTime: 2021-03-04 21:56:43
  * @FilePath: \Leetcode\136.Single Number.cpp
  */
 /*
@@ -14,21 +14,53 @@
 
 // @lc code=start
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <set>
+#include <queue>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:     16ms    BEAT: 94.96%    O(n) = n
+MEMORY: 16.4MB    BEAT: 95.57%    O(n) = 1
+USED TIME: 01:39
+LAST EDIT TIME: 2021年3月4日21:56:0
+Description: 
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int tmp = nums[0];
+        for (int i = 1; i < nums.size(); i ++) {
+            tmp ^= nums[i];
+        } 
+        return tmp;
+    }
+};
+
 
 /*
 Accept: 17.81 22.10
 异或
 */
 
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int ans = nums[0];
-        for (int i = 1; i < nums.size(); i++){
-            ans = ans ^ nums[i];
-        }
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int ans = nums[0];
+//         for (int i = 1; i < nums.size(); i++){
+//             ans = ans ^ nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
 // @lc code=end
 
---
+++ 232.Implement Queue using Stacks.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+
+    }
+    
+    /** Get the front element. */
+    int peek() {
+
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
