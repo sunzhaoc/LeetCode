59e047d97751177b688ca4e28de3f51776a4a086
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-09 14:33:50 +0800

    master

---
+++ 63.Unique Paths II.cpp
@@ -0,0 +1,80 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-09 13:59:06
+ * @LastEditTime: 2021-03-09 14:14:30
+ * @FilePath: \Leetcode\63.Unique Paths II.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:     4ms    BEAT: 81.73%    O(n) = mn
+MEMORY: 7.7MB    BEAT: 50.89%    O(n) = m
+USED TIME: 12:42
+LAST EDIT TIME: 2021年3月9日14:13:30
+Description: Easy DP
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] =  obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 1; i < m; i ++) {
+            dp[i][0] = obstacleGrid[i][0] == 0 ? dp[i - 1][0] : 0;
+        }
+        for (int i = 1; i < n; i ++) {
+            dp[0][i] = obstacleGrid[0][i] == 0 ? dp[0][i - 1] : 0;
+        }
+        for (int i = 1; i < m; i ++) {
+            for (int j = 1; j < n; j ++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+
+
+int main() {
+    Solution sol;
+    vector<vector<int>> obstacleGrid = {{0,0,0}, {0,1,0}, {0,0,0}};
+    vector<vector<int>> obstacleGrid = {{0,1}, {0,0}};
+    int ans = sol.uniquePathsWithObstacles(obstacleGrid);
+    cout << ans << endl; 
+    system("pause");
+    return 0;
+}
+
+// @lc code=end
+
---
+++ 75.Sort Colors.cpp
@@ -0,0 +1,101 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-09 14:18:33
+ * @LastEditTime: 2021-03-09 14:33:00
+ * @FilePath: \Leetcode\75.Sort Colors.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=75 lang=cpp
+ *
+ * [75] 颜色分类
+ */
+
+// @lc code=start
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+
+
+/*
+RESULT: Accept
+TIME:   ms    BEAT: %    O(n) = 
+MEMORY: MB    BEAT: %    O(n) = 
+USED TIME: 
+LAST EDIT TIME: 
+Description: 
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int p1 = 0, p2 = nums.size() - 1;
+        for (int i = 0; i < p2; i ++) {
+            while (i <= p2 && nums[i] == 2) {
+                swap(nums[i], nums[p2]);
+                p2 --;
+            }
+        }
+    }
+};
+
+
+/*
+RESULT: Accept
+TIME:   4ms    BEAT: 52.86%    O(n) = n
+MEMORY: 8MB    BEAT: 75.58%    O(n) = 1
+USED TIME: 02:46
+LAST EDIT TIME: 2021年3月9日14:25:43
+Description: 先统计每个颜色的个数。
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int map[3] = {0, 0, 0};
+        for (int num: nums) {
+            map[num] ++;
+        }
+        for (int i = 0; i < map[0]; i ++) {
+            nums[i] = 0;
+        }
+        for (int i = map[0]; i < map[0] + map[1]; i ++) {
+            nums[i] = 1;
+        }
+        for (int i = map[0] + map[1]; i < nums.size(); i ++) {
+            nums[i] = 2;
+        }
+    }
+};
+
+
+/*
+RESULT: Accept
+TIME:     0ms    BEAT: 100.00%    O(n) = nlogn
+MEMORY: 8.1MB    BEAT:  54.90%    O(n) = logn
+USED TIME: 00:45
+LAST EDIT TIME: 2021年3月9日14:20:40
+Description: 干嘛不用sort？我又不傻。
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
+
+// @lc code=end
+
